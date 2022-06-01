e4f58bdfbac8f7d38a897e682e73388d0a1f2bd7
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-06-01 17:45:20 -0500

    Create Tools View

---
+++ app/src/main/java/com/example/codexbettingapp/ToolsView.kt
@@ -0,0 +1,57 @@
+package com.example.codexbettingapp
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.codexbettingapp.ui.theme.CodexBettingTheme
+import com.example.codexbettingapp.ui.theme.CodexBlack
+
+@Composable
+fun ToolsView() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(CodexBlack)
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .background(CodexBlack),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            CodexNavigationBar()
+
+            Column(horizontalAlignment = Alignment.Start) {
+                Text("Herramientas", color = Color.White,  fontSize = 28.sp, modifier = Modifier.padding(horizontal = 15.dp))
+
+                InformativeCardView(title = "Calculadora", description = "Realiza los procedimientos matématicos con ayuda de la calculadora") {
+
+                }
+
+                InformativeCardView(title = "Verificador de apuesta", description = "Verifica que tu apuesta no tenga nungún error") {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToolsViewPreview() {
+    CodexBettingTheme {
+        ToolsView()
+    }
+}
